68a59c342e3d505fd823a8e30d97bfa593db6b63
Steven Bustos <stvnbvsts@gmail.com>
2025-08-18 22:09:48 +0200

    modules for contact and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ContactModule } from './contact/contact.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ContactModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
