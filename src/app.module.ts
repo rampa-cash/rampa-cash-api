b74ae549ad52c55296eb5e00eccbce11e6177461
Steven Bustos <stvnbvsts@gmail.com>
2025-09-21 13:55:55 +0200

    move folders to domain folder for better order

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
-import { ContactModule } from './contact/contact.module';
-import { UserModule } from './user/user.module';
+import { ContactModule } from './domain/contact/contact.module';
+import { UserModule } from './domain/user/user.module';
 import { getDatabaseConfig } from './config/database.config';
 import { getLoggerConfig } from './config/logger.config';
 
